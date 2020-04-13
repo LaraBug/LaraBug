33453efd288ac90c74225ff70e5c423443953819
nickurt <nickurt@users.noreply.github.com>
2020-04-13 15:59:38 +0200

    larabug/js-controller beautifier

--- src/Http/Controllers/LaraBugReportController.php
+++ src/Http/Controllers/LaraBugReportController.php
@@ -6,24 +6,25 @@ use Illuminate\Http\Request;
 
 class LaraBugReportController
 {
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\Routing\ResponseFactory
+     */
     public function report(Request $request)
     {
+        /** @var \LaraBug\LaraBug $laraBug */
         $laraBug = app('larabug');
 
-        $data = [
-            'file' => $request->input('file'),
-            'line' => $request->input('line'),
-            'message' => $request->input('message'),
-            'stack' => $request->input('stack'),
-            'url' => $request->input('url'),
-        ];
-
         $laraBug->handle(
-            new \ErrorException(
-                $request->input('message')
-            ),
+            new \ErrorException($request->input('message')),
             'javascript',
-            $data
+            [
+                'file' => $request->input('file'),
+                'line' => $request->input('line'),
+                'message' => $request->input('message'),
+                'stack' => $request->input('stack'),
+                'url' => $request->input('url'),
+            ]
         );
 
         return response('ok', 200);
