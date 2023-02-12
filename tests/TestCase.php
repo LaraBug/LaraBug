10baf09ba4332718128f012d7b4544708c874a56
Lloric Mayuga Garcia <lloricode@gmail.com>
2023-02-12 12:26:44 +0800

    DElete dead code
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,15 +7,6 @@ use Illuminate\Foundation\Application;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
 
     /**
      * @param Application $app
