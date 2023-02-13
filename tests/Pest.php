73b80537158d9680e7352f734742a28ed0b81ff8
Lloric Mayuga Garcia <lloricode@gmail.com>
2023-02-13 10:04:20 +0800

    Add Http::preventStrayRequests() when testing
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use LaraBug\Tests\TestCase;
+use Illuminate\Support\Facades\Http;
 
 uses(TestCase::class)
+    ->beforeEach(fn () => Http::preventStrayRequests())
     ->in(__DIR__);
