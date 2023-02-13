9ffb957f7967b6148f04c7c5db58f872a9b686a2
Lloric Mayuga Garcia <lloricode@gmail.com>
2023-02-13 10:09:41 +0800

    Use Http::preventStrayRequests() in laravel 9
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,5 +6,9 @@ use LaraBug\Tests\TestCase;
 use Illuminate\Support\Facades\Http;
 
 uses(TestCase::class)
-    ->beforeEach(fn () => Http::preventStrayRequests())
+    ->beforeEach(function () {
+        if (version_compare(app()::VERSION, '9.0.0', '>=')) {
+            Http::preventStrayRequests();
+        }
+    })
     ->in(__DIR__);
